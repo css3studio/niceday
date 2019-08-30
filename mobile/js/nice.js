5201d3f04e327f283f2531fce0eb8ca29c7ac61a
html5exe <css3studio@gmail.com>
2019-08-30 16:01:13 +0800

    초기 업로드

---
+++ mobile/js/nice.js
@@ -0,0 +1,23 @@
+/*
+Theme Name: give a nice day
+Author: css3studio / slowalk
+Version:1.0
+*/
+
+$(document).ready(function() {
+	
+	//전쟁속 아이와 팔찌 슬라이드
+	$('#sec5 .slide').slick({
+		initialSlide:0,
+		speed: 300,
+		dots:true,
+        arrows:false
+	});
+	$('#sec6 .slider ul').slick({
+        initialSlide:0,
+        speed: 300,
+        dots:true
+	});
+ 
+});
+
